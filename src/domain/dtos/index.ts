7c2ce8390a75318d6498f680c51965a48d90b8b1
alberto8812 <101751073+alberto8812@users.noreply.github.com>
2024-04-14 14:16:42 -0500

    create dto for create

---
+++ src/domain/dtos/index.ts
@@ -0,0 +1,2 @@
+export { CreateTodoDto } from "./todo/create-todo.dto";
+
--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -3,6 +3,7 @@
 
 import { Request, Response } from "express";
 import { prisma } from "../../data/postgres";
+import { CreateTodoDto } from "../../domain/dtos";
 
 
 export class TodosController {
@@ -24,11 +25,12 @@ export class TodosController {
   }
 
   public async createTodo (req: Request, res: Response) {
-    const { text } = req.body;
-    if (!text) res.status(400).json({ error: "text isrequiered" });
+    const [error,createTodoDto]=  CreateTodoDto.create(req.body);
+    if(error) return res.status(400).json({error})
+
 
     const todo=await prisma.todos.create({
-        data:{text}
+        data:createTodoDto!
     });
 
 
@@ -70,10 +72,7 @@ export class TodosController {
     const findProduct = await prisma.todos.findUnique({where:{id}})
     if (!findProduct)
       return res.status(404).json({ error: `Tod with id ${id} not found` });
-
     const deleteProduct =await prisma.todos.delete({where:{id}});
-    if (!findProduct)
-      return res.status(404).json({ error: `Tod with id ${id} not found` });
     res.json(deleteProduct)
   }
 }
